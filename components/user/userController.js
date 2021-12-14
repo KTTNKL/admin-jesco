610a79cd4681aab4d978527f59065186f6130564
TrungNam <huynhtrungnam2001@gmail.com>
2021-12-14 16:38:49 +0700

    [Admin] pagination user list

--- components/user/userController.js
+++ components/user/userController.js
@@ -1,21 +1,21 @@
 const userService = require("./userService");
 
 exports.list = async function (req, res) {
-  // let page;
-  // if (req.query.page === undefined) {
-  //   page = 1;
-  // } else if (req.query.page < 0) {
-  //   page = 1;
-  // } else {
-  //   page = parseInt(req.query.page);
-  // }
-  // const users = await userService.listUsers(page);
-  // let totalPage = await userService.totalUserNum();
-  // totalPage = Math.ceil(totalPage / 4);
-  const users = await userService.listUsers();
+  let page;
+  if (req.query.page === undefined) {
+    page = 1;
+  } else if (req.query.page < 0) {
+    page = 1;
+  } else {
+    page = parseInt(req.query.page);
+  }
+  const users = await userService.listUsers(page);
+  let totalPage = await userService.totalUserNum();
+  totalPage = Math.ceil(totalPage / 4);
+  //const users = await userService.listUsers();
   res.render("userList", {
-    // page: page, // Current Page
-    // totalPage, // Total Page
+    page: page, // Current Page
+    totalPage, // Total Page
     users: users
   });
 };
--- components/user/userService.js
+++ components/user/userService.js
@@ -7,6 +7,14 @@ const PAGE_SIZE = 4;
 //   const Skip = (page - 1) * PAGE_SIZE;
 //   return User.find({}).skip(Skip).limit(PAGE_SIZE);
 // };
-exports.listUsers = () => User.find().lean()
+
+exports.listUsers = async (page) => {
+  
+  const Skip = (page - 1) * PAGE_SIZE;
+  page = parseInt(page);
+  let listUser = await User.find({}).skip(Skip).limit(PAGE_SIZE);
+  return listUser;
+};
+//exports.listUsers = () => User.find().lean()
 exports.totalUserNum = () => User.countDocuments();
 exports.viewOne = (id) => User.findById(id).lean();
