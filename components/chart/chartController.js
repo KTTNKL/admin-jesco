4c37fb147ca2e2f8584c84f26a020ba53543c6db
TrungNam <huynhtrungnam2001@gmail.com>
2021-12-18 17:09:35 +0700

    [Admin] statistic by week

--- components/chart/chartController.js
+++ components/chart/chartController.js
@@ -1,7 +1,7 @@
 //const userService = require("./userService");
 const chartService = require("./chartService");
 
-exports.list = async function (req, res) {
+exports.statisticByDay = async function (req, res) {
   const order = await chartService.listOrder();
   const statisticByDay = [0, 0, 0, 0, 0, 0, 0];
 
@@ -38,12 +38,62 @@ exports.list = async function (req, res) {
   }
   console.log(day1.getMonth)
   let Xaxist = [ { day: day1.getDate(), month: day1.getMonth() },
-            { day: day2.getDate(), month: day2.getMonth() },
-            { day: day3.getDate(), month: day3.getMonth() },
-            { day: day4.getDate(), month: day4.getMonth() },
-            { day: day5.getDate(), month: day5.getMonth() },
-            { day: day6.getDate(), month: day6.getMonth() },
-            { day: day7.getDate(), month: day7.getMonth() }
+            { day: day2.getDate(), month: day2.getMonth() + 1 },
+            { day: day3.getDate(), month: day3.getMonth() + 1 },
+            { day: day4.getDate(), month: day4.getMonth() + 1 },
+            { day: day5.getDate(), month: day5.getMonth() + 1 },
+            { day: day6.getDate(), month: day6.getMonth() + 1 },
+            { day: day7.getDate(), month: day7.getMonth() + 1 }
   ];
-  res.render("chart/views/statistic",{Xaxist: Xaxist, statisticByDay: statisticByDay});
+  res.render("chart/views/statisticByDay",{Xaxist: Xaxist, statisticByDay: statisticByDay});
+};
+
+
+exports.statisticByWeek = async function (req, res) {
+  const order = await chartService.listOrder();
+  const statisticByWeek = [0, 0, 0, 0];
+
+  var curr = new Date;
+  var firstdayWeek4 = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+  var lastdayWeek4 = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+
+
+  curr = new Date(curr);
+  curr.setDate(curr.getDate() - 7)
+  var firstdayWeek3 = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+  var lastdayWeek3 = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+
+  curr = new Date(curr);
+  curr.setDate(curr.getDate() - 7)
+  var firstdayWeek2 = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+  var lastdayWeek2 = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+
+  curr = new Date(curr);
+  curr.setDate(curr.getDate() - 7)
+  var firstdayWeek1 = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+  var lastdayWeek1 = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+
+  let week = [
+    {fday: firstdayWeek1.getDate(), fmonth: firstdayWeek1.getMonth() + 1, lday: lastdayWeek1.getDate(), lmonth: lastdayWeek1.getMonth() + 1},
+    {fday: firstdayWeek2.getDate(), fmonth: firstdayWeek2.getMonth() + 1, lday: lastdayWeek2.getDate(), lmonth: lastdayWeek2.getMonth() + 1},
+    {fday: firstdayWeek3.getDate(), fmonth: firstdayWeek3.getMonth() + 1, lday: lastdayWeek3.getDate(), lmonth: lastdayWeek3.getMonth() + 1},
+    {fday: firstdayWeek4.getDate(), fmonth: firstdayWeek4.getMonth() + 1, lday: lastdayWeek4.getDate(), lmonth: lastdayWeek4.getMonth() + 1}
+  ]
+
+  for (let i = 0; i < order.length; i++) {
+
+    let date = new Date(order[i].DateOfPurchase);
+    
+    if((date.getTime() >= firstdayWeek1.getTime() && date.getTime() <= lastdayWeek1.getTime()))
+      statisticByWeek[0]++;
+    else if((date.getTime() >= firstdayWeek2.getTime() && date.getTime() <= lastdayWeek2.getTime()))
+      statisticByWeek[1]++;
+    else if((date.getTime() >= firstdayWeek3.getTime() && date.getTime() <= lastdayWeek3.getTime()))
+      statisticByWeek[2]++;
+    else if((date.getTime() >= firstdayWeek4.getTime() && date.getTime() <= lastdayWeek4.getTime()))
+      statisticByWeek[3]++;
+  }
+
+  console.log(statisticByWeek);
+  res.render("chart/views/statisticByWeek", {Xaxist: week, statisticByWeek: statisticByWeek});
 };
--- components/chart/index.js
+++ components/chart/index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 const chartController = require("./chartController");
 /* GET home page. */
-router.get("/byDay", chartController.list);
+router.get("/byDay", chartController.statisticByDay);
+router.get("/byWeek", chartController.statisticByWeek);
 
 module.exports = router;
 
